6dbd490a7f9a8c0525cf6959f0e4b6238390f6af
Martin Masson <martinmasson03@gmail.com>
2021-06-05 00:07:40 +0200

    started reading the book up to chapter 4

---
+++ src/chap3.rs
@@ -0,0 +1,9 @@
+#[allow(dead_code)]
+pub fn fibonacci(x: u32) -> u32
+{
+    if x == 0 || x == 1 {
+        x
+    } else {
+        fibonacci(x-1) + fibonacci(x-2)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,7 @@
-struct Court
-{
-    name: String,
-    occupation: Vec<i32>
-}
-
-struct Club
-{
-    courts: Vec<Court>,
-    open: i32,
-    close: i32  
-}
-
-impl Club
-{
-    // Constructor
-    fn new_club(opening: i32, closure: i32) -> Club
-    {
-        Club {
-            open: opening,
-            close: closure,
-            courts: Vec::new()
-        }
-    }
-
-    // Methods
-    fn add_court(&mut self, name: &str) -> bool
-    {
-        self.courts.push(
-            Court {
-                name: name.to_string(),
-                occupation: Vec::new()
-            }
-        );
-        return true;
-    }
-}
+//mod guessing_game;
+mod chap3;
 
 fn main()
 {
-    let my_club = Club::new_club(8, 20);
+    
 }
